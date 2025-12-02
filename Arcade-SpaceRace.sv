e2b859e68e9f9df1865a390d49f01034a49b3f99
jackyangantelope <jack@retroremake.co>
2025-12-02 11:53:14 +0800

    modify frame

--- Arcade-SpaceRace.sv
+++ Arcade-SpaceRace.sv
@@ -58,6 +58,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -194,6 +196,8 @@ assign VGA_F1 = 0;
 assign VGA_SCALER = 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
